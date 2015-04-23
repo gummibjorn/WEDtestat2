97c6443b32f693f0b55077f3b1f6c01a47c8578d
tourn <latzer.daniel@gmail.com>
2015-04-23 13:40:22 +0200

    Implement voting

--- public/app.js
+++ public/app.js
@@ -49,12 +49,48 @@
         });
         //console.log(html);
         content.html(html);
+        registerVotes();
         setTimeout(refresh, 2000);
       }
     });
+  }
+
+  function registerVotes(){
+    /*
+    $('.upvote').each(function(){
+      $(this).click(function(){
+        var id = $(this).parent('div').parent('div').attr('id');
+        console.log("Upvote " + id);
+      });
+    });
+    */
+
+    content.children("div").each(function(){
+      var that = $(this);
+      var id = that.attr('id');
+      that.find(".upvote").click(function(){
+        vote(id, "up");
+      });
+      that.find(".downvote").click(function(){
+        vote(id, "down");
+      });
+    });
+
+  }
+
+  function vote(id, dir){
+    $.post('/links/'+id+'/'+dir, {
+      success: function(){
+        var $score = $('#'+id).find('.score');
+        var score = parseInt($score.html());
+        score += (dir === 'up' ? 1 : -1);
+        $score.html(score);
+      }
+    });
+  }
 
   Handlebars.registerHelper("formatDate", function(datetime) {
     //TODO: this only works in new chrome/ff, add a backwards compatible solution
     return new Date(datetime).toLocaleString("de-CH", {timeZone: "Europe/Zurich"});
-  }); }
+  }); 
 })(jQuery);
