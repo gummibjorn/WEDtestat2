1261a601649a1ffd4e54808266ce188c6a586fd9
tourn <latzer.daniel@gmail.com>
2015-05-06 14:09:04 +0200

    Add login information

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var storage = require('../storage.js');
 
 exports.index = function(req, res){
-        res.render('index', { title: 'SawIt',  links: storage.getAll() });
+        res.render('index', { title: 'SawIt', user: req.session.user });
 };
