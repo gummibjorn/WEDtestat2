53bb7de636f6819cf5783f00723db31f100e29a8
tourn <latzer.daniel@gmail.com>
2015-05-06 15:10:41 +0200

    Add some more titles

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var storage = require('../storage.js');
 
+var titles = ['SawIt', 'SeenIt', 'ReadIt', 'LinkIt', 'ScrewIt', 'EatIt', 'MegabIt', 'NoobIt', 'SproinkIt', 'ChairIt', 'RebootIt', 'CloseIt', 'BookmarkIt', 'LoveIt', 'HateIt', 'dontcareaboutIt', 'BathsIt', 'ReloadIt', 'BelieveIt', 'PayIt', 'PostIt'];
+
 exports.index = function(req, res){
-        res.render('index', { title: 'SawIt', user: req.session.user });
+  var title = titles[Math.floor(Math.random()*titles.length)];
+  res.render('index', { title: title, user: req.session.user });
 };
